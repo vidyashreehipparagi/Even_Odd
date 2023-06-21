60dc944c9b3f4ba3683e50409d88256d5a336fdb
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-21 16:15:40 +0530

    Add project files.

---
+++ Even_Odd/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Enter Number");
+num = Convert.ToInt32(Console.ReadLine());
+ 
+String result = (num%2==0) ? "Even" : "Odd";
+Console.WriteLine(result);
